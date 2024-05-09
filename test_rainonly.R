0e2b9121b5bf06a2c591516665dc49b0ffee1a46
Kim Cressman <kim@catbirdstats.com>
2024-05-09 08:48:20 -0500

    more raindl testing

--- test_rainonly.R
+++ test_rainonly.R
@@ -1,2 +1,2 @@
-source(here::here("R", "raindl.R"))
-test <- read_importrain(curyr = 2021, catch_pixels = catch_pixels)
+source(here::here("R", "raindl2.R"))
+test <- read_importrain(curyr = 2021, catch_pixels = catch_pixels, mos = 1:3, quiet = F)
